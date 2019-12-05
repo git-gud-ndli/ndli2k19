895a82973295eb2b4241c898444a45bb3d30621b
ludovicm67 <ludovicmuller1@gmail.com>
2019-12-05 21:36:25 +0100

    db: tags

--- back/migrations/000001_initial.up.sql
+++ back/migrations/000001_initial.up.sql
@@ -15,7 +15,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- users
-CREATE TABLE IF NOT EXISTS api.users (
+CREATE TABLE IF NOT EXISTS internal.users (
   user_id serial PRIMARY KEY,
   claim jsonb NOT NULL,
   created_at timestamptz NOT NULL DEFAULT NOW(),
@@ -23,54 +23,70 @@ CREATE TABLE IF NOT EXISTS api.users (
 );
 
 CREATE TRIGGER set_timestamp
-    BEFORE UPDATE ON api.users
-    FOR EACH ROW
-    EXECUTE PROCEDURE internal.trigger_set_timestamp();
+  BEFORE UPDATE ON internal.users
+  FOR EACH ROW
+  EXECUTE PROCEDURE internal.trigger_set_timestamp();
 
 -- posts
-CREATE TABLE IF NOT EXISTS api.posts (
+CREATE TABLE IF NOT EXISTS internal.posts (
   post_id serial PRIMARY KEY,
-  user_id integer REFERENCES api.users(user_id) ON DELETE SET NULL,
+  user_id integer REFERENCES internal.users(user_id) ON DELETE SET NULL,
   content text NOT NULL,
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
 
 CREATE TRIGGER set_timestamp
-    BEFORE UPDATE ON api.posts
-    FOR EACH ROW
-    EXECUTE PROCEDURE internal.trigger_set_timestamp();
+  BEFORE UPDATE ON internal.posts
+  FOR EACH ROW
+  EXECUTE PROCEDURE internal.trigger_set_timestamp();
 
 -- questions
-CREATE TABLE IF NOT EXISTS api.questions (
+CREATE TABLE IF NOT EXISTS internal.questions (
   question_id serial PRIMARY KEY,
-  post_id integer NOT NULL REFERENCES api.posts(post_id) ON DELETE CASCADE,
+  post_id integer NOT NULL REFERENCES internal.posts(post_id) ON DELETE CASCADE,
   title text NOT NULL,
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
 
 CREATE TRIGGER set_timestamp
-    BEFORE UPDATE ON api.questions
-    FOR EACH ROW
-    EXECUTE PROCEDURE internal.trigger_set_timestamp();
+  BEFORE UPDATE ON internal.questions
+  FOR EACH ROW
+  EXECUTE PROCEDURE internal.trigger_set_timestamp();
 
 -- answers
-CREATE TABLE IF NOT EXISTS api.answers (
+CREATE TABLE IF NOT EXISTS internal.answers (
   answer_id serial PRIMARY KEY,
-  post_id integer NOT NULL REFERENCES api.posts(post_id) ON DELETE CASCADE,
-  question_id integer NOT NULL REFERENCES api.questions(question_id) ON DELETE CASCADE
+  post_id integer NOT NULL REFERENCES internal.posts(post_id) ON DELETE CASCADE,
+  question_id integer NOT NULL REFERENCES internal.questions(question_id) ON DELETE CASCADE
 );
 
 CREATE TRIGGER set_timestamp
-    BEFORE UPDATE ON api.answers
-    FOR EACH ROW
-    EXECUTE PROCEDURE internal.trigger_set_timestamp();
+  BEFORE UPDATE ON internal.answers
+  FOR EACH ROW
+  EXECUTE PROCEDURE internal.trigger_set_timestamp();
+
+-- tags
+CREATE TABLE IF NOT EXISTS internal.tags (
+  tag_id serial PRIMARY KEY,
+  name text,
+  slug text NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT NOW(),
+  updated_at timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_timestamp
+  BEFORE UPDATE ON internal.tags
+  FOR EACH ROW
+  EXECUTE PROCEDURE internal.trigger_set_timestamp();
+
+-- questions <-> tags
+CREATE TABLE IF NOT EXISTS internal.questions_tags (
+  question_id integer NOT NULL REFERENCES internal.questions(question_id) ON DELETE CASCADE,
+  tag_id integer NOT NULL REFERENCES internal.tags(tag_id) ON DELETE CASCADE
+);
 
 -- permissions
-GRANT USAGE ON SCHEMA api TO anonymous;
-GRANT USAGE ON SCHEMA internal TO anonymous;
-GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA api TO anonymous;
-GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA internal TO anonymous;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA api TO anonymous;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA internal TO anonymous;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA api TO anonymous;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA internal TO anonymous;
