ab0da8b245bcce385cdbf9459537bae9778912cd
nargyriou <nicolas.argyriou95@gmail.com>
2019-12-05 21:26:07 +0100

    Aucune différence

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import FetchWrapper from "./FetchWrapper";
+import vuetify from "./plugins/vuetify";
+import { createProvider } from "./vue-apollo";
+window.api = new FetchWrapper();
+
+Vue.config.productionTip = false;
+//let deferredPrompt = null;
+window.addEventListener("beforeinstallprompt", e => {
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  //  deferredPrompt = e;
+  // store.dispatch('notifications/setInstallPrompt', deferredPrompt)
+});
+
+window.addEventListener("appinstalled", () => {
+  // document.querySelector("#install-app-btn").style.display = "none";
+});
+
+Vue.filter("summary", function(text, stop, clamp) {
+  if (!text) {
+    return "";
+  }
+  return text.slice(0, stop) + (stop < text.length ? clamp || "..." : "");
+});
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/store/push.module.js
@@ -0,0 +1,121 @@
+import localforage from "localforage";
+import { base64DecToArr } from "../helpers";
+
+const configurePushSub = sub => {
+  if (sub) {
+    const key = sub.getKey("p256dh");
+    const token = sub.getKey("auth");
+    const contentEncoding = (PushManager.supportedContentEncodings || [
+      "aesgcm"
+    ])[0];
+    // eslint-disable-next-line
+    return api.post("/push/subscribe", {
+      endpoint: sub.endpoint,
+      publicKey: key
+        ? btoa(String.fromCharCode.apply(null, new Uint8Array(key)))
+        : null,
+      authToken: token
+        ? btoa(String.fromCharCode.apply(null, new Uint8Array(token)))
+        : null,
+      contentEncoding
+    });
+  }
+  return navigator.serviceWorker.ready
+    .then(reg => {
+      // convert VAPID public key
+      let publicKey = base64DecToArr(process.env.VUE_APP_VAPID_KEY);
+      return reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicKey
+      });
+    })
+    .then(newSub => {
+      const key = newSub.getKey("p256dh");
+      const token = newSub.getKey("auth");
+      const contentEncoding = (PushManager.supportedContentEncodings || [
+        "aesgcm"
+      ])[0];
+      // eslint-disable-next-line
+      return api.post("/push/subscribe", {
+        endpoint: newSub.endpoint,
+        publicKey: key
+          ? btoa(String.fromCharCode.apply(null, new Uint8Array(key)))
+          : null,
+        authToken: token
+          ? btoa(String.fromCharCode.apply(null, new Uint8Array(token)))
+          : null,
+        contentEncoding
+      });
+    });
+};
+
+const unsubscribeToPush = () => {
+  return navigator.serviceWorker.ready
+    .then(reg => reg.pushManager.getSubscription())
+    .then(async sub => {
+      let { endpoint } = sub;
+      await sub.unsubscribe();
+      // eslint-disable-next-line
+      return api.post("/push/unsubscribe", { endpoint });
+    });
+};
+
+export const push = {
+  namespaced: true,
+  state: {
+    pushEnabled: false
+  },
+  mutations: {
+    setPush: (state, value) => {
+      state.pushEnabled = !!value;
+    }
+  },
+  actions: {
+    checkPushPermission: ({ dispatch }) => {
+      localforage.getItem("push_enabled").then(val => {
+        if (val === true) {
+          dispatch("setPushStatus", true);
+        }
+      });
+    },
+    setPushStatus: ({ commit }, value) => {
+      if (!("Notification" in window && navigator.serviceWorker)) {
+        // unsupported
+        commit("setPush", false);
+        localforage.setItem("push_enabled", false);
+      }
+      // eslint-disable-next-line
+      if (api.token) {
+        if (value) {
+          Notification.requestPermission().then(async value => {
+            // permission granted
+            if (value === "granted") {
+              let sub = await navigator.serviceWorker.ready.then(reg => {
+                return reg.pushManager.getSubscription();
+              });
+              if (sub) {
+                commit("setPush", true);
+              }
+              configurePushSub(sub)
+                .then(() => {
+                  commit("setPush", true);
+                  localforage.setItem("push_enabled", true);
+                })
+                .catch(() => {});
+            }
+          });
+        } else {
+          unsubscribeToPush()
+            .then(() => {
+              commit("setPush", false);
+              localforage.setItem("push_enabled", false);
+            })
+            .catch(() => {});
+        }
+      }
+    }
+  },
+  getters: {
+    push: state => state.pushEnabled
+  }
+};
