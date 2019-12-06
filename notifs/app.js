b78bdf6a6ca68ba929beb499aaf9b177bca26959
Quentin Gliech <quentingliech@gmail.com>
2019-12-06 06:13:29 +0100

    notifs: add p256dh and auth keys

--- notifs/app.js
+++ notifs/app.js
@@ -8,20 +8,18 @@ webpush.setVapidDetails(
 );
 
 const client = new Client();
-console.log('Paul est lent !!!');
 
 client.connect();
 client.query('LISTEN notif_new_question');
-client.query('LISTEN notif_new_tag');
 
 client.on('notification', msg => {
   switch (msg.channel) {
     case 'notif_new_question':
       client.query(
-        'SELECT endpoint FROM internal.devices AS dev INNER JOIN internal.question_sub AS q ON q.user_id = dev.user_id WHERE q.question_id = $1',
+        'SELECT endpoint, p256dh, auth FROM internal.devices AS dev INNER JOIN internal.question_sub AS q ON q.user_id = dev.user_id WHERE q.question_id = $1',
         [msg.payload],
         (err, res) => {
-          res.rows.forEach(e => {
+          res.rows.forEach(({ endpoint, p256dh, auth }) => {
             console.log(e);
             const payload = JSON.stringify({
               title: 'Nicolas le pire DJ',
@@ -29,27 +27,13 @@ client.on('notification', msg => {
             });
 
             webpush
-              .sendNotification(e, payload)
-              .then(result => console.log(result))
-              .catch(e => console.log(e.stack));
-          });
-        },
-      );
-      break;
-    case 'notif_new_tag':
-      client.query(
-        'SELECT endpoint FROM internal.devices AS dev INNER JOIN internal.tag_sub AS t ON s.user_id = dev.user_id WHERE t.tag_id = $1',
-        [msg.payload],
-        (err, res) => {
-          res.rows.forEach(e => {
-            console.log(e);
-            const payload = JSON.stringify({
-              title: 'Nicolas le pire DJ',
-              body: "C'est le pire",
-            });
-
-            webpush
-              .sendNotification(e, payload)
+              .sendNotification({
+                endpoint,
+                keys: {
+                  p256dh,
+                  auth
+                }
+              }, payload)
               .then(result => console.log(result))
               .catch(e => console.log(e.stack));
           });
