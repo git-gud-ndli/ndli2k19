1c7fd5a955b83743162c86eb0829bdc36b2a4534
ludovicm67 <ludovicmuller1@gmail.com>
2019-12-06 03:28:04 +0100

    back: subs

---
+++ back/migrations/000007_subs.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS internal.tag_sub;
+DROP TABLE IF EXISTS internal.question_sub;
+DROP IF EXISTS VIEW api.question_sub;
+DROP IF EXISTS VIEW api.tag_sub;
+DROP IF EXISTS FUNCTION api.add_question_sub;
+DROP IF EXISTS FUNCTION api.add_tag_sub;
