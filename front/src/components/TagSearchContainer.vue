f87bd0502513048f0e04d718847bc15c42d3ddb0
Nicolas Streng <nicolas@nicolasstr.eu>
2019-12-06 05:09:21 +0100

    Ajout du conteneur de recherche des tags

---
+++ front/src/components/TagSearchContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+        
+    </v-row>
+    <v-row>
+        <SearchBar />
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "tag-search-container",
+  data() {
+      return {
+          tagsAppliques = []
+      }
+  }
+};
+</script>
