e5c9595a8680a50516c768332648089841d2201e
Paul <rahveiz@gmail.com>
2019-12-06 02:23:55 +0100

    add question

--- front/src/components/QuestionForm.vue
+++ front/src/components/QuestionForm.vue
@@ -14,11 +14,19 @@
           <v-container>
             <v-row>
               <v-col cols="12">
-                <v-text-field label="Titre*" required></v-text-field>
+                <v-text-field
+                  label="Titre*"
+                  required
+                  v-model="title"
+                ></v-text-field>
               </v-col>
 
               <v-col cols="12">
-                <v-text-field label="Question" required></v-text-field>
+                <v-text-field
+                  label="Question"
+                  required
+                  v-model="content"
+                ></v-text-field>
               </v-col>
 
               <v-col cols="12" sm="6">
@@ -37,7 +45,7 @@
           <v-btn color="blue darken-1" text @click="dialog = false"
             >Close</v-btn
           >
-          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+          <v-btn color="blue darken-1" text @click="submitQuestion">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -45,12 +53,40 @@
 </template>
 
 <script>
+import gql from "graphql-tag";
 export default {
   name: "QuestionForm",
   data: () => {
     return {
-      dialog: false
+      dialog: false,
+      title: "",
+      content: ""
     };
+  },
+  methods: {
+    submitQuestion() {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation($title: String!, $content: String!) {
+              addQuestion(input: { title: $title, content: $content }) {
+                question {
+                  questionId
+                  title
+                }
+              }
+            }
+          `,
+          // Parameters
+          variables: {
+            title: this.title,
+            content: this.content
+          }
+        })
+        .then(() => {
+          this.dialog = false;
+        });
+    }
   }
 };
 </script>
